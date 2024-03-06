bd77dde38958367da2456d68b3dc7a92018dca18
Thibault Walterspieler <twalterspieler@pm.me>
2024-03-06 10:54:31 +0100

    style: adjust styling in content-wrapper and work display for better layout and visibility

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -16,7 +16,7 @@ export const SideMenu: FC<Props> = ({ children, isInner }) => {
 	return (
 		<>
 			{!isMenuOpen && (
-				<div className="absolute top-10 right-4 lg:hidden bg-eerie-dark/30 z-50 backdrop-blur rounded p-2 justify-between">
+				<div className="absolute top-10 right-4 lg:hidden bg-eerie-dark/30 z-50 backdrop-blur rounded p-2">
 					<Command onClick={openMenu} size={24} />
 				</div>
 			)}
@@ -28,7 +28,7 @@ export const SideMenu: FC<Props> = ({ children, isInner }) => {
 
 			<ScrollArea
 				className={cn(
-					"bg-eerie-dark lg:flex lg:flex-col border-r border-grey z-40",
+					"bg-eerie-dark lg:flex lg:flex-col border-r border-grey z-40 justify-between",
 					!isMenuOpen && "hidden",
 					isInner ? "lg:w-72 xl:w-72" : "lg:w-60 xl:w-72",
 				)}
