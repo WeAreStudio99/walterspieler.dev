a7d27647ee674ef35cc42c050c0f01468866e003
Thibault Walterspieler <twalterspieler@pm.me>
2024-03-08 17:17:31 +0100

    fix: change x to y

--- src/app/[lang]/works/[uid]/template.tsx
+++ src/app/[lang]/works/[uid]/template.tsx
@@ -12,9 +12,9 @@ import { ReactNode } from "react";
 export default function Template({ children }: { children: ReactNode }) {
 	return (
 		<motion.main
-			animate={{ y: 0, opacity: 1 }}
-			exit={{ y: -100, opacity: 0 }}
-			initial={{ y: 0, opacity: 0 }}
+			animate={{ x: 0, opacity: 1 }}
+			exit={{ x: -10, opacity: 0 }}
+			initial={{ x: -300, opacity: 0 }}
 			transition={{ duration: 0.4 }}
 		>
 			{children}
