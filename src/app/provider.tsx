ff03037985fdf83ff905310651aa3c5e87babe79
Thibault Walterspieler <twalterspieler@pm.me>
2024-03-15 14:05:07 +0100

    fix: change posthog api host

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,7 +4,8 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
 	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+		api_host: `${process.env.BASE_URL}/ingest`,
+		ui_host: `https://eu.posthog.com`,
 		capture_pageview: false,
 		persistence: "localStorage",
 	});
@@ -16,6 +17,14 @@ export function PHProvider({
 	children: React.ReactNode;
 }) {
 	if (process.env.NODE_ENV === "development") {
+		return <>{children}</>;
+	}
+
+	const isInternalUser =
+		typeof window !== "undefined" &&
+		window.localStorage.getItem("POSTHOG_INTERNAL_USER") === "TRUE";
+
+	if (isInternalUser) {
 		return <>{children}</>;
 	}
 
