3d0c070200938a5cd30983bdeb4338165240d830
Thibault Walterspieler <twalterspieler@pm.me>
2024-03-16 18:37:13 +0100

    fix: fix title width

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,4 +1,5 @@
 import { ScrollArea } from "@/components/ScrollArea";
+import { H1 } from "@/components/Typography";
 import { Locale } from "@/lib/i18n/types";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
@@ -78,8 +79,27 @@ const HomeLang: FC<Props> = async (props) => {
 			<ScrollArea className="flex flex-col">
 				<div className="content-wrapper">
 					<div className="content">
+						<H1 className="max-w-36 mb-4 md:mb-8 md:max-w-full">
+							Thibault Walterspieler
+						</H1>
 						<SliceZone components={components} slices={page.data.slices} />
 					</div>
+					{/* <div
+						aria-hidden="true"
+						className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48 animate-in fade-in"
+					>
+						<svg
+							className="aspect-[801/1036] w-[50.0625rem] opacity-10"
+							viewBox="0 0 200 200"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M20.3,-28.9C33,-27.9,54.6,-35.9,53,-32.6C51.5,-29.3,26.9,-14.6,17.9,-5.2C8.8,4.2,15.3,8.4,19.1,15.6C22.8,22.8,23.9,32.9,20.2,46.9C16.6,60.8,8.3,78.5,-3.4,84.3C-15,90.2,-30.1,84.2,-32,69.3C-34,54.4,-22.8,30.5,-23.6,17.5C-24.3,4.5,-37,2.2,-47.5,-6.1C-58,-14.4,-66.4,-28.8,-65.7,-41.9C-65,-55,-55.2,-66.8,-42.7,-68C-30.3,-69.1,-15.1,-59.6,-5.7,-49.8C3.8,-40,7.6,-29.9,20.3,-28.9Z"
+								fill="#7ACCB8"
+								transform="translate(100 100)"
+							/>
+						</svg>
+					</div> */}
 				</div>
 			</ScrollArea>
 		</>
