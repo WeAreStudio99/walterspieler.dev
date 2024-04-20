a05be501a3d369d944ddeeafabed7330cf4e21bf
Thibault Walterspieler <twalterspieler@pm.me>
2024-04-20 10:45:31 +0200

    chore: add @tensorflow/tfjs

---
+++ src/app/[lang]/lab/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+import { H1 } from "@/components/Common/Typography";
+
+const LabPage: FC = async () => {
+  // const model = await loadLayersModel();
+
+  return (
+    <>
+      <H1>lab</H1>
+    </>
+  );
+};
+
+export default LabPage;
