55bb2b9411d444c843a0c0cca7acb79ae9dea7e5
Thibault Walterspieler <twalterspieler@pm.me>
2024-12-02 15:06:12 +0100

    fix: move layouts to appropriate folder

--- src/app/(frontend)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-import ContentWrapper from "@/components/Common/ContentWrapper";
-
-const HomePage: FC = async () => {
-  return <ContentWrapper></ContentWrapper>;
-};
-
-export default HomePage;
