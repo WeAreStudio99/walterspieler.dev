dbbabfe86c35e59e5c37b187f0bbbb959cf14cd5
Thibault Walterspieler <twalterspieler@pm.me>
2024-12-05 12:03:52 +0100

    chore: remove useless config for images

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.prismic.io",
-      },
-    ],
-  },
   redirects: async () => {
     return [
       {
